6012793a981de52fef4e9a1b63d4734379316e70
itshabhab <root@LAPTOP-RA6CILPB.localdomain>
2022-04-08 02:45:19 +0300

    puts firt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main - Entry point
  * puts message
  * Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
