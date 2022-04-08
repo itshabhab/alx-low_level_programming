1c6c3c422162271659f48955b317fc83daa80b1b
itshabhab <root@LAPTOP-RA6CILPB.localdomain>
2022-04-08 18:22:51 +0300

    patience is a virtue

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char abc;
+for (abc = 'a'; abc <= 'z'; abc++)
+putchar(abc);
+putchar('\n');
+return (0);
+}
