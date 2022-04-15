009a8374f5b4c22eb489eb1b1162b947f2cf0988
itshabhab <root@LAPTOP-RA6CILPB>
2022-04-15 03:58:43 +0300

    all works with patience

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,17 +9,15 @@
 
 void print_number(int n)
 {
-unsigned int n;
-if (c < 0)
-{
-_putchar('-');
-m = -n;
-}
-else
-{
-m = n;
-}
-if (m / 10 != 0)
-print_number(m / 10);
-_putchar((m % 10) + '0');
+	unsigned int x;
+
+	x = n;
+	if (n < 0)
+	{
+		_putchar(45);
+			x = -n;
+	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
