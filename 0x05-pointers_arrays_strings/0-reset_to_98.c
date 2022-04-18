6dcf02fef410bfb76b3267fa47b6d6bc1a31824f
itshabhab <root@LAPTOP-RA6CILPB>
2022-04-18 20:37:11 +0300

    reet 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - entry point
+ * @n: print the value
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+ This is a readme.md file for a c pointer and array task 
+
