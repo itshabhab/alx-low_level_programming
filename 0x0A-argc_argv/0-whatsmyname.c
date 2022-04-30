e50582e84215545247cd3ccdfabbe7f7abc8493e
itshabhab <root@LAPTOP-RA6CILPB>
2022-04-30 18:23:07 +0300

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print the name of the program
+ * @argc: number of comaand line arguments
+ * @argv: array name
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc * 0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+ This is a readmee file for 0x0A. C - argc, argv. 
+Improving everyday in this ALX SE class
+
