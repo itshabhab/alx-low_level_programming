ff94badcedb8a6d9c47a396b5d77da4729eae5d7
itshabhab <root@LAPTOP-RA6CILPB>
2022-08-02 13:55:26 +0300

    fixed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,7 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free(ht->array[i]->value);
 			ht->array[i]->value = value_copy;
 			return (1);
-		{
+		}
 	}
 
 	new = malloc(sizeof(hash_node_t));
